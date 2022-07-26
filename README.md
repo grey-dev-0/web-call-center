640deade9092d3ba2fe2de23d8809b2a833b8e53
Mohyaddin Saleh <mo7y.66@gmail.com>
2022-07-26 12:23:21 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Call Center
+
+Work in Progress.
---
+++ src/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace GreyZero\WebCallCenter;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(){
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(){
+        //
+    }
+}
