9ab6e3a025ce8738b098da6e0c8846cc8b2662ac
Mohyaddin Saleh <mo7y.66@gmail.com>
2022-08-16 08:34:10 +0300

    Registered API routes with sanctum authentication.

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider{
     /**
+     * @var string $namespace Router's namespace of the web call center application.
+     */
+    private $namespace = 'GreyZero\WebCallCenter\Controllers';
+
+    /**
      * Register any application services.
      *
      * @return void
@@ -49,13 +54,45 @@ class AppServiceProvider extends ServiceProvider{
         \Route::aliasMiddleware('wcc', \GreyZero\WebCallCenter\AppMiddleware::class);
     }
 
+    /**
+     * Registers web call center routes for web and external API users e.g. mobile applications.
+     *
+     * @return void
+     */
     private function registerRoutes(){
-        $namespace = 'GreyZero\WebCallCenter\Controllers';
-        $router = \Route::middleware(['web', config('web-call-center.middleware')]);
-        if(!is_null($prefix = config('web-call-center.prefix')))
-            $router->prefix($prefix);
-        $router->namespace($namespace)->group(__DIR__.'/../../routes/app.php');
-        \Route::group((!empty($prefix)? compact('prefix') : []) + compact('namespace') + ['middleware' => 'web'],
+        $this->registerApplicationRoutes();
+        $this->registerApplicationRoutes(false);
+        $this->registerAuthenticationRoutes();
+    }
+
+    /**
+     * Registers user authentication routes for accessing web call center.
+     *
+     * @return void
+     */
+    private function registerAuthenticationRoutes(){
+        $prefix = config('web-call-center.prefix');
+        \Route::group((!empty($prefix)? compact('prefix') : []) + ['namespace' => $this->namespace, 'middleware' => 'web'],
             __DIR__.'/../../routes/auth.php');
+    }
+
+    /**
+     * Registers the application routes of web call center.
+     *
+     * @param bool $web Determines whether to register application's routes for web or external API usage.
+     * @return void
+     */
+    private function registerApplicationRoutes($web = true){
+        $prefix = config('web-call-center.prefix');
+        if($web)
+            $middleware = ['web', config('web-call-center.middleware')];
+        else{
+            $middleware = ['api', 'auth:sanctum'];
+            $prefix = is_null($prefix)? 'api' : "api/$prefix";
+        }
+        $router = \Route::middleware($middleware);
+        if(!is_null($prefix))
+            $router->prefix($prefix);
+        $router->namespace($this->namespace)->group(__DIR__.'/../../routes/app.php');
     }
 }
